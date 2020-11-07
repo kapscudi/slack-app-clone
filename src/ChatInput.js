3a1daa2d250b3fda7e14fb60e8147414d874e876
Digikane <62964446+digikane@users.noreply.github.com>
2020-11-07 11:35:00 +0100

    edited

--- src/ChatInput.js
+++ src/ChatInput.js
@@ -1,25 +1,25 @@
-import React, { useState } from "react";
-import db from "./firebase";
-import "./chatinput.css";
-import { useStateValue } from "./StateProvider";
-import firebase from "firebase";
+import React, { useState } from 'react';
+import db from './firebase';
+import './chatinput.css';
+import { useStateValue } from './StateProvider';
+import firebase from 'firebase';
 
 function ChatInput({ channelName, channelId }) {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
   const [{ user }] = useStateValue();
 
   const sendMessage = (e) => {
     e.preventDefault();
 
     if (channelId) {
-      db.collection("rooms").doc(channelId).collection("messages").add({
+      db.collection('rooms').doc(channelId).collection('messages').add({
         message: input,
         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
         user: user.displayName,
         userimage: user.photoURL,
       });
     }
-    setInput("");
+    setInput('');
   };
 
   return (
